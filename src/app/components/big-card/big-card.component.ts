b0ae0afa2736b1b8683ec9db2d852112ba13e709
pepemf <jpedromoco@gmail.com>
2023-11-29 20:30:02 -0300

    Update app component and add home page

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+  @Input()
+  public photoCover: string = 'https://placehold.co/600x400';
+  @Input()
+  public title: string = 'Title Placeholder';
+  @Input()
+  public description: string =
+    'Description placeholder. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  public photoCover: string = 'https://placehold.co/300x200';
+  @Input()
+  public title: string = 'Title Placeholder';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
